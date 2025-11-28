995b8d57fc95d7dc95b10ee41d6294b593ffb352
Stefan Santesson <stefan@aaa-sec.com>
2025-11-28 18:34:11 +0100

    Setup for AoC 2025

---
+++ src/test/java/com/aaasec/aoc2025/Aoc2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaasec.aoc2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aoc2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
