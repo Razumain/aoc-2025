a63db4fac834bcfdb6f36a7d797e17ed475a1937
Stefan Santesson <stefan@aaa-sec.com>
2025-12-01 10:12:23 +0100

    Fixing solve buttons

--- src/main/java/com/aaasec/aoc2025/solutions/Day1Solution.java
+++ src/main/java/com/aaasec/aoc2025/solutions/Day1Solution.java
@@ -5,6 +5,7 @@ import com.aaasec.aoc2025.solve.Solution;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day1Solution extends Solution {
   public Day1Solution(final int day, final String title, final File inputFile) throws IOException {
@@ -13,13 +14,51 @@ public class Day1Solution extends Solution {
 
   @Override
   public void solvePart1(List<String> input) {
-    String result = "";
-    log.add("Result part 1: %s", result);
+
+
+    int pos = 50;
+    int passw = 0;
+    for (String line : input) {
+      int step = Integer.parseInt(line.substring(1));
+      boolean right = line.startsWith("R");
+      pos = right ? pos + step : pos - step;
+      pos = pos % 100;
+      if (pos == 0) {
+        passw++;
+      }
+
+    }
+
+    log.add("Result part 1: %s", passw);
   }
 
   @Override
   public void solvePart2(List<String> input) {
-    String result = "";
-    log.add("Result part 2: %s", result);
+    int pos = 50;
+    int passw = 0;
+    for (String line : input) {
+      int step = Integer.parseInt(line.substring(1));
+      boolean right = line.startsWith("R");
+      if (right) {
+        pos = pos + step;
+        if (pos >= 100) {
+          passw = passw + pos/100;
+        }
+      } else {
+        if (pos == 0) {
+          pos = 100;
+        }
+        pos = pos - step;
+        if (pos <= 0) {
+          passw = passw + Math.abs(pos/100) +1 ;
+        }
+      }
+      pos = pos % 100;
+      if (pos < 0) {
+        pos = 100 + pos;
+      }
+
+    }
+    log.add("Result part 2: %s", passw);
   }
 }
